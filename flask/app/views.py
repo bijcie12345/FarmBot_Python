6b804d27eda4df0471e3ef1663f1ffa94bf94829
Your Name <danielkruk306@gmail.com>
2020-10-28 15:12:21 +0100

    a

--- flask/app/views.py
+++ flask/app/views.py
@@ -11,3 +11,7 @@ def index():
         return f"Hello from {app_name} running in a Docker container behind Nginx!"
 
     return "Hello from Flask"
+
+@app.route("/dev", methods=['GET'])
+def home():
+    return "<h1>Distant reading archive</h1><p>This site is a prototpye API for ...</p>"
